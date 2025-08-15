5990c4462567dafc8fb0b5b70b00d95b4529baeb
Velasco-Tecnosinergia <jair.velasco@tecnosinergia.com>
2025-08-15 17:45:19 -0600

    feat(sidebar): update navigation and styling

--- login-app/src/components/Card.tsx
+++ login-app/src/components/Card.tsx
@@ -5,8 +5,8 @@ interface CardProps {
 
 export default function Card({ title, value }: CardProps) {
   return (
-    <div className="bg-white p-4 rounded-lg shadow">
-      <h3 className="text-gray-600">{title}</h3>
+    <div className="bg-white p-4 rounded-lg shadow hover:bg-indigo-950 text-indigo-950 hover:text-white">
+      <h3 className="font-bold">{title}</h3>
       <p className="text-2xl font-bold">{value}</p>
     </div>
   );
--- login-app/src/layouts/DashboardLayout.tsx
+++ login-app/src/layouts/DashboardLayout.tsx
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-slate-200">
+    <div className="flex min-h-screen bg-indigo-100">
       <Sidebar />
       <main className="flex-1 p-6">{children}</main>
     </div>
