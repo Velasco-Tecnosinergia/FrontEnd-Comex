0bcd7871a83da195f47e8692f4e89ec6983a7b90
Velasco-Tecnosinergia <jair.velasco@tecnosinergia.com>
2025-08-15 11:05:48 -0600

    feat(dashboard): update main layout and widgets

--- login-app/src/components/Dashboard.tsx
+++ login-app/src/components/Dashboard.tsx
@@ -2,31 +2,36 @@
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } from 'recharts';
 
 const barData = [
-  { name: 'Ene', ventas: 4000, gastos: 2400 },
-  { name: 'Feb', ventas: 3000, gastos: 1398 },
-  { name: 'Mar', ventas: 2000, gastos: 9800 },
-  { name: 'Abr', ventas: 2780, gastos: 3908 },
-  { name: 'May', ventas: 1890, gastos: 4800 },
-  { name: 'Jun', ventas: 2390, gastos: 3800 },
-  { name: 'Jul', ventas: 3490, gastos: 4300 },
+  { name: 'Lunes', Llegaron: 5400, Dejaron: 5400 , Presentes: 0},
+  { name: 'Martes', Llegaron: 6000, Dejaron: 6000 , Presentes: 0},
+  { name: 'Miercoles', Llegaron: 5700, Dejaron: 5700 , Presentes: 0},
+  { name: 'Jueves', Llegaron: 6100, Dejaron: 6100 , Presentes: 0},
+  { name: 'Viernes', Llegaron: 7500, Dejaron: 7500 , Presentes: 0},
+  { name: 'Sabado', Llegaron: 7000, Dejaron: 7000 , Presentes: 0},
+  { name: 'Domingo', Llegaron: 4000, Dejaron: 3700 , Presentes: 300},
 ];
 
 const lineData = [
-  { name: 'Ene', clientes: 400 },
-  { name: 'Feb', clientes: 300 },
-  { name: 'Mar', clientes: 200 },
-  { name: 'Abr', clientes: 278 },
-  { name: 'May', clientes: 189 },
-  { name: 'Jun', clientes: 239 },
-  { name: 'Jul', clientes: 349 },
+  { name: 'Ene', Clientes: 100000 },
+  { name: 'Feb', Clientes: 101250 },
+  { name: 'Mar', Clientes: 90000 },
+  { name: 'Abr', Clientes: 95000 },
+  { name: 'May', Clientes: 105300 },
+  { name: 'Jun', Clientes: 80000 },
+  { name: 'Jul', Clientes: 85000 },
+  { name: 'Ago', Clientes: 87500 },
+  { name: 'Sep', Clientes: 99000 },
+  { name: 'Oct', Clientes: 100500 },
+  { name: 'Nov', Clientes: 102500 },
+  { name: 'Dic', Clientes: 5000 },
 ];
 
 export default function Dashboard() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex min-h-screen bg-slate-200">
       {/* Sidebar */}
       <aside className="w-64 bg-indigo-700 text-white p-5">
-        <h2 className="text-2xl font-bold mb-8">Mi Dashboard</h2>
+        <h2 className="text-2xl font-bold mb-8">Dashboard COMEX</h2>
         <nav className="space-y-4">
           <a href="#" className="block hover:text-indigo-200">Inicio</a>
           <a href="#" className="block hover:text-indigo-200">Reportes</a>
@@ -39,42 +44,43 @@ export default function Dashboard() {
         {/* Cards */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-6">
           <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="text-gray-600">Ventas</h3>
-            <p className="text-2xl font-bold">$6,280</p>
+            <h3 className="text-gray-600">Camara 1</h3>
+            <p className="text-2xl font-bold">1,002,500</p>
           </div>
           <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="text-gray-600">Clientes</h3>
-            <p className="text-2xl font-bold">1,259</p>
+            <h3 className="text-gray-600">Camara 2</h3>
+            <p className="text-2xl font-bold">1,100,900</p>
           </div>
           <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="text-gray-600">Pedidos</h3>
-            <p className="text-2xl font-bold">2,434</p>
+            <h3 className="text-gray-600">Camara 3</h3>
+            <p className="text-2xl font-bold">1,050,900</p>
           </div>
           <div className="bg-white p-4 rounded-lg shadow">
-            <h3 className="text-gray-600">Rating</h3>
-            <p className="text-2xl font-bold">8.5</p>
+            <h3 className="text-gray-600">Camara 4</h3>
+            <p className="text-2xl font-bold">987,500</p>
           </div>
         </div>
 
         {/* Gráficas */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-1 gap-6">
           <div className="bg-white p-4 rounded-lg shadow h-80">
-            <h3 className="text-gray-700 mb-4">Ventas vs Gastos</h3>
-            <ResponsiveContainer width="100%" height="100%">
+            <h3 className="text-gray-700 mb-4">Clientes Presentes</h3>
+            <ResponsiveContainer className="" width="100%" height="100%">
               <BarChart data={barData}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="ventas" fill="#4f46e5" />
-                <Bar dataKey="gastos" fill="#f97316" />
+                <Bar dataKey="Llegaron" fill="#1A8057" />
+                <Bar dataKey="Dejaron" fill="#DEB500" />
+                <Bar dataKey="Presentes" fill="#9C1A15" />
               </BarChart>
             </ResponsiveContainer>
           </div>
 
           <div className="bg-white p-4 rounded-lg shadow h-80">
-            <h3 className="text-gray-700 mb-4">Crecimiento de Clientes</h3>
+            <h3 className="text-gray-700 mb-4">Crientes totales</h3>
             <ResponsiveContainer width="100%" height="100%">
               <LineChart data={lineData}>
                 <CartesianGrid strokeDasharray="3 3" />
@@ -82,11 +88,12 @@ export default function Dashboard() {
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="clientes" stroke="#10b981" strokeWidth={3} />
+                <Line type="monotone" dataKey="Clientes" stroke="#032263" strokeWidth={3} />
               </LineChart>
             </ResponsiveContainer>
           </div>
         </div>
+
       </main>
     </div>
   );
